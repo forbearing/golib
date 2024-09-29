ea96b40e0647286c956362a8e851ab24bab4e7f8
hybfkuf <fredjoseph863@gmail.com>
2024-09-29 17:00:43 +0800

    add GormScanner, add index for field `created_at`

--- model/model.go
+++ model/model.go
@@ -138,7 +138,7 @@ type Base struct {
 
 	CreatedBy      string    `json:"created_by,omitempty" schema:"created_by"`
 	UpdatedBy      string    `json:"updated_by,omitempty" schema:"updated_by"`
-	CreatedAt      time.Time `json:"created_at,omitempty" schema:"-"`
+	CreatedAt      time.Time `json:"created_at,omitempty" schema:"-" gorm:"index"`
 	UpdatedAt      time.Time `json:"updated_at,omitempty" schema:"-"`
 	Remark         *string   `json:"remark,omitempty" gorm:"size:10240" schema:"-"` // 如果需要支持 PATCH 更新,则必须是指针类型
 	Order          *uint     `json:"order,omitempty" schema:"-"`
@@ -285,4 +285,37 @@ func (gs *GormStrings) Scan(value any) error {
 func (gs GormStrings) Value() (driver.Value, error) {
 	// It will return "", if gs is nil or empty string.
 	return strings.Trim(strings.Join(gs, ","), ","), nil
+}
+
+// GormScannerWrapper converts object to GormScanner that can be used in GORM.
+// WARN: you must pass pointer to object.
+func GormScannerWrapper(object any) *GormScanner {
+	return &GormScanner{Object: object}
+}
+
+type GormScanner struct {
+	Object any
+}
+
+func (g *GormScanner) Scan(value any) (err error) {
+	if value == nil {
+		return nil
+	}
+	switch v := value.(type) {
+	case string:
+		err = json.Unmarshal(util.StringToBytes(v), g.Object)
+	case []byte:
+		err = json.Unmarshal(v, g.Object)
+	default:
+		err = errors.New("unsupported type")
+	}
+	return err
+}
+
+func (g *GormScanner) Value() (driver.Value, error) {
+	data, err := json.Marshal(g.Object)
+	if err != nil {
+		return nil, err
+	}
+	return util.BytesToString(data), nil
 }
