7a0cc20e3be13a037eac429a3c173d8aa381b997
hybfkuf <fredjoseph863@gmail.com>
2024-10-07 09:30:21 +0800

    chore: add doc

--- bootstrap/handler.go
+++ bootstrap/handler.go
@@ -23,14 +23,19 @@ func runHandlers() {
 	}
 }
 
+// Exit will call all exit handlers and then exit.
 func Exit(code int) {
 	runHandlers()
 	os.Exit(code)
 }
 
+// RegisterExitHandler append custom exit handler, the handler will be invoked by `Exit(int)` function.
+// first handler will be called first
 func RegisterExitHandler(handler func()) {
 	handlers = append(handlers, handler)
 }
+
+// DeferExitHandler same as RegisterExitHandler, but last handler will be called first.
 func DeferExitHandler(handler func()) {
 	handlers = append([]func(){handler}, handlers...)
 }
