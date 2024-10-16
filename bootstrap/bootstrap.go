4ede7a350cd3016f695ae8f73692e4598b11e2d3
hybfkuf <fredjoseph863@gmail.com>
2024-10-16 21:09:06 +0800

    add `Boostrap`

---
+++ bootstrap/bootstrap.go
@@ -0,0 +1,73 @@
+package bootstrap
+
+import (
+	"github.com/forbearing/golib/config"
+	"github.com/forbearing/golib/database/cache"
+	"github.com/forbearing/golib/database/mysql"
+	"github.com/forbearing/golib/database/postgres"
+	"github.com/forbearing/golib/database/redis"
+	"github.com/forbearing/golib/database/sqlite"
+	"github.com/forbearing/golib/logger/logrus"
+	"github.com/forbearing/golib/logger/zap"
+	"github.com/forbearing/golib/metrics"
+	"github.com/forbearing/golib/minio"
+	"github.com/forbearing/golib/rbac"
+	"github.com/forbearing/golib/router"
+	"github.com/forbearing/golib/service"
+	"github.com/forbearing/golib/task"
+	"github.com/forbearing/golib/util"
+)
+
+var keys = []string{
+	util.GetFunctionName(config.Init),
+	util.GetFunctionName(zap.Init),
+	util.GetFunctionName(logrus.Init),
+	util.GetFunctionName(metrics.Init),
+	util.GetFunctionName(cache.Init),
+	util.GetFunctionName(sqlite.Init),
+	util.GetFunctionName(postgres.Init),
+	util.GetFunctionName(mysql.Init),
+	util.GetFunctionName(redis.Init),
+	util.GetFunctionName(rbac.Init),
+	util.GetFunctionName(service.Init),
+	util.GetFunctionName(minio.Init),
+	util.GetFunctionName(router.Init),
+	util.GetFunctionName(task.Init),
+}
+
+func Bootstrap() error {
+	// config.Init,
+	// InitConfig,
+	// pkgzap.Init,
+	// logrus.Init,
+	// metrics.Init,
+	// cache.Init,
+	// // mysql.Init,
+	// // sqlite.Init,
+	// postgres.Init,
+	// redis.Init,
+	// rbac.Init,
+	// service.Init,
+	// minio.Init,
+	// router.Init,
+	// task.Init,
+
+	Register(
+		config.Init,
+		zap.Init,
+		logrus.Init,
+		metrics.Init,
+		cache.Init,
+		// mysql.Init,
+		// sqlite.Init,
+		postgres.Init,
+		redis.Init,
+		rbac.Init,
+		service.Init,
+		minio.Init,
+		router.Init,
+		task.Init,
+	)
+
+	return Init()
+}
