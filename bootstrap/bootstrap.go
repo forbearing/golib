a839d7de0178c07e7ab6647ebc4e419096d1ddf3
hybfkuf <fredjoseph863@gmail.com>
2024-11-04 12:36:37 +0800

    chg(bootstrap): bootstrap mongo

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -13,48 +13,15 @@ import (
 	"github.com/forbearing/golib/lru"
 	"github.com/forbearing/golib/metrics"
 	"github.com/forbearing/golib/minio"
+	"github.com/forbearing/golib/mongo"
 	"github.com/forbearing/golib/mqtt"
 	"github.com/forbearing/golib/rbac"
 	"github.com/forbearing/golib/router"
 	"github.com/forbearing/golib/service"
 	"github.com/forbearing/golib/task"
-	"github.com/forbearing/golib/util"
 )
 
-var keys = []string{
-	util.GetFunctionName(config.Init),
-	util.GetFunctionName(zap.Init),
-	util.GetFunctionName(logrus.Init),
-	util.GetFunctionName(metrics.Init),
-	util.GetFunctionName(cmap.Init),
-	util.GetFunctionName(sqlite.Init),
-	util.GetFunctionName(postgres.Init),
-	util.GetFunctionName(mysql.Init),
-	util.GetFunctionName(redis.Init),
-	util.GetFunctionName(rbac.Init),
-	util.GetFunctionName(service.Init),
-	util.GetFunctionName(minio.Init),
-	util.GetFunctionName(router.Init),
-	util.GetFunctionName(task.Init),
-}
-
 func Bootstrap() error {
-	// config.Init,
-	// InitConfig,
-	// pkgzap.Init,
-	// logrus.Init,
-	// metrics.Init,
-	// cache.Init,
-	// // mysql.Init,
-	// // sqlite.Init,
-	// postgres.Init,
-	// redis.Init,
-	// rbac.Init,
-	// service.Init,
-	// minio.Init,
-	// router.Init,
-	// task.Init,
-
 	Register(
 		config.Init,
 		zap.Init,
@@ -65,8 +32,9 @@ func Bootstrap() error {
 		sqlite.Init,
 		postgres.Init,
 		mysql.Init,
-		elastic.Init,
 		redis.Init,
+		elastic.Init,
+		mongo.Init,
 		minio.Init,
 		mqtt.Init,
 		rbac.Init,
