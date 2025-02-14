64b229d287a582bc00109d496e4ed98ae19207ff
hybfkuf <fredjoseph863@gmail.com>
2025-02-14 13:39:22 +0800

    chg(avltree): update WithNodeFormatter signature

--- ds/tree/avltree/avltree.go
+++ ds/tree/avltree/avltree.go
@@ -2,6 +2,7 @@ package avltree
 
 import (
 	"cmp"
+	"fmt"
 	"strings"
 
 	"github.com/forbearing/golib/ds/types"
@@ -17,7 +18,7 @@ type Tree[K comparable, V any] struct {
 
 	safe          bool
 	mu            types.Locker
-	nodeFormatter func(*Node[K, V]) string
+	nodeFormatter func(K, V) string
 }
 
 // New creates and returns a AVL tree.
@@ -729,13 +730,13 @@ func (t *Tree[K, V]) String() string {
 	sb.WriteString("AVLTree\n")
 	nodeFormatter := t.nodeFormatter
 	if nodeFormatter == nil {
-		nodeFormatter = func(n *Node[K, V]) string { return n.String() }
+		nodeFormatter = func(k K, v V) string { return fmt.Sprintf("%v", k) }
 	}
 	t.output(t.root, "", true, &sb, nodeFormatter)
 	return sb.String()
 }
 
-func (t *Tree[K, V]) output(node *Node[K, V], prefix string, isTail bool, sb *strings.Builder, nodeFormatter func(*Node[K, V]) string) {
+func (t *Tree[K, V]) output(node *Node[K, V], prefix string, isTail bool, sb *strings.Builder, nodeFormatter func(K, V) string) {
 	if node.Children[1] != nil {
 		newPrefix := prefix
 		if isTail {
@@ -753,7 +754,7 @@ func (t *Tree[K, V]) output(node *Node[K, V], prefix string, isTail bool, sb *st
 		sb.WriteString("╭── ")
 	}
 
-	sb.WriteString(nodeFormatter(node))
+	sb.WriteString(nodeFormatter(node.Key, node.Value))
 	sb.WriteString("\n")
 
 	if node.Children[0] != nil {
