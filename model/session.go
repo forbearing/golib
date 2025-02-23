8f14f3dbc3806b5497396b76f6d085bf80f1db63
hybfkuf <fredjoseph863@gmail.com>
2025-02-23 20:49:14 +0800

    add model/session.go

---
+++ model/session.go
@@ -0,0 +1,53 @@
+package model
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"strings"
+)
+
+func init() {
+	Register[*Session]()
+}
+
+type Session struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	UserId       string `json:"user_id"`
+	Username     string `json:"username"`
+	SessionId    string `json:"session_id"`
+
+	// TODO: 统一起来，使用 model.UserAgent
+	Platform       string `json:"platform"`
+	OS             string `json:"os"`
+	EngineName     string `json:"engine_name"`
+	EngineVersion  string `json:"engine_version"`
+	BrowserName    string `json:"browser_name"`
+	BrowserVersion string `json:"browser_version"`
+
+	Base
+}
+
+func (s *Session) initDefault() error {
+	s.ID = s.id()
+	return nil
+}
+
+func (s *Session) CreateBefore() error { return s.initDefault() }
+func (s *Session) UpdateBefore() error { return s.initDefault() }
+func (s *Session) DeleteBefore() error {
+	s.ID = s.id()
+	return nil
+}
+
+func (s *Session) id() string {
+	parts := []string{
+		s.UserId,
+		s.Platform,
+		s.OS,
+		s.EngineName,
+		s.BrowserName,
+	}
+	hash := sha256.Sum256([]byte(strings.Join(parts, ":")))
+	return hex.EncodeToString(hash[:8])
+}
