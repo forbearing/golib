159c5985cc0385a1c4a184ab1232890b17636087
hybfkuf <fredjoseph863@gmail.com>
2025-02-23 21:44:25 +0800

    enhance model/User

--- model/user.go
+++ model/user.go
@@ -1,9 +1,7 @@
 package model
 
 import (
-	"strconv"
-
-	"github.com/cockroachdb/errors"
+	"strings"
 )
 
 type User struct {
@@ -44,18 +42,62 @@ type User struct {
 	Base
 }
 
-// CreateBefore check whether user mobile is valid.
-func (u *User) CreateBefore() error {
-	if len(u.Mobile) != 0 {
-		if len(u.Mobile) != 11 {
-			return errors.New("mobile number length must be 11")
-		}
-		if _, err := strconv.Atoi(u.Mobile); err != nil {
-			return err
-		}
-	}
+func (u *User) GetAfter() error  { return u.mask() }
+func (u *User) ListAfter() error { return u.mask() }
+
+func (u *User) mask() error {
+	u.Mobile = maskMobile(u.Mobile)
+	u.Name = maskUsername(u.Name)
+	u.Email = maskEmail(u.Email)
 	return nil
 }
 
-// GetAfter clean the fields value of Password, RePassword, NewPassword .
-func (u *User) GetAfter() error { return u.ListAfter() }
+// maskMobile 隐去手机号中间 4 位地区码, 如 155****8888
+func maskMobile(phone string) string {
+	if n := len(phone); n >= 8 {
+		return phone[:n-8] + "****" + phone[n-4:]
+	}
+	return phone
+}
+
+// maskEmail 隐藏邮箱ID的中间部分 zhang@go-mall.com ---> z***g@go-mall.com
+func maskEmail(address string) string {
+	strings.LastIndex(address, "@")
+	id := address[0:strings.LastIndex(address, "@")]
+	domain := address[strings.LastIndex(address, "@"):]
+
+	if len(id) <= 1 {
+		return address
+	}
+	switch len(id) {
+	case 2:
+		id = id[0:1] + "*"
+	case 3:
+		id = id[0:1] + "*" + id[2:]
+	case 4:
+		id = id[0:1] + "**" + id[3:]
+	default:
+		masks := strings.Repeat("*", len(id)-4)
+		id = id[0:2] + masks + id[len(id)-2:]
+	}
+
+	return id + domain
+}
+
+// maskUsername 保留姓名首末位 如：张三--->张* 赵丽颖--->赵*颖 欧阳娜娜--->欧**娜
+func maskUsername(realName string) string {
+	runeRealName := []rune(realName)
+	if n := len(runeRealName); n >= 2 {
+		if n == 2 {
+			return string(append(runeRealName[0:1], rune('*')))
+		} else {
+			count := n - 2
+			newRealName := runeRealName[0:1]
+			for temp := 1; temp <= count; temp++ {
+				newRealName = append(newRealName, rune('*'))
+			}
+			return string(append(newRealName, runeRealName[n-1]))
+		}
+	}
+	return realName
+}
