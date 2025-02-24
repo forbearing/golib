a4cc154864aaf6ec125d77b2f7c9fda9b117d7c5
hybfkuf <fredjoseph863@gmail.com>
2025-02-24 11:33:43 +0800

    refactor(jwt): remove accessTokenCache and refreshTokenCache

--- jwt/session.go
+++ jwt/session.go
@@ -5,39 +5,39 @@ import (
 	"github.com/forbearing/golib/model"
 )
 
-func setToken(accessToken, refreshToken string, s *model.Session) {
-	if s == nil {
-		return
-	}
-	accessTokenCache.Add(accessToken, s)
-	refreshTokenCache.Add(refreshToken, s)
-	database.Database[*model.Session]().Update(s)
-}
-
-func removeToken(accessToken, refreshToken string) {
-	sessions := make([]*model.Session, 0)
-	if len(accessToken) > 0 {
-		accessTokenCache.Remove(accessToken)
-		if err := database.Database[*model.Session]().WithLimit(-1).WithQuery(&model.Session{AccessToken: accessToken}).WithSelect("id").List(&sessions); err == nil {
-			database.Database[*model.Session]().WithPurge().Delete(sessions...)
-		}
-	}
-
-	if len(refreshToken) > 0 {
-		refreshTokenCache.Remove(refreshToken)
-		if err := database.Database[*model.Session]().WithLimit(-1).WithQuery(&model.Session{RefreshToken: refreshToken}).WithSelect("id").List(&sessions); err == nil {
-			database.Database[*model.Session]().WithPurge().Delete(sessions...)
-		}
-	}
-}
-
-func GetAccessToken(accessToken string) (*model.Session, bool) {
-	return accessTokenCache.Get(accessToken)
-}
-
-func GetRefreshToken(refreshToken string) (*model.Session, bool) {
-	return refreshTokenCache.Get(refreshToken)
-}
+// func setToken(accessToken, refreshToken string, s *model.Session) {
+// 	if s == nil {
+// 		return
+// 	}
+// 	accessTokenCache.Add(accessToken, s)
+// 	refreshTokenCache.Add(refreshToken, s)
+// 	database.Database[*model.Session]().Update(s)
+// }
+//
+// func removeToken(accessToken, refreshToken string) {
+// 	sessions := make([]*model.Session, 0)
+// 	if len(accessToken) > 0 {
+// 		accessTokenCache.Remove(accessToken)
+// 		if err := database.Database[*model.Session]().WithLimit(-1).WithQuery(&model.Session{AccessToken: accessToken}).WithSelect("id").List(&sessions); err == nil {
+// 			database.Database[*model.Session]().WithPurge().Delete(sessions...)
+// 		}
+// 	}
+//
+// 	if len(refreshToken) > 0 {
+// 		refreshTokenCache.Remove(refreshToken)
+// 		if err := database.Database[*model.Session]().WithLimit(-1).WithQuery(&model.Session{RefreshToken: refreshToken}).WithSelect("id").List(&sessions); err == nil {
+// 			database.Database[*model.Session]().WithPurge().Delete(sessions...)
+// 		}
+// 	}
+// }
+//
+// func GetAccessToken(accessToken string) (*model.Session, bool) {
+// 	return accessTokenCache.Get(accessToken)
+// }
+//
+// func GetRefreshToken(refreshToken string) (*model.Session, bool) {
+// 	return refreshTokenCache.Get(refreshToken)
+// }
 
 func setSession(userId string, s *model.Session) {
 	if len(userId) == 0 || s == nil {
