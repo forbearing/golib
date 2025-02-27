42371efad340598b81f174c79e2657fc436e8cd9
hybfkuf <fredjoseph863@gmail.com>
2025-02-27 11:28:56 +0800

    enh(task): support reigster task before or after bootstrap.Boostrap()

--- task/task.go
+++ task/task.go
@@ -15,7 +15,7 @@ var (
 	tasks []*task
 	mu    sync.Mutex
 
-	taskFlag uint64
+	inited bool
 )
 
 type task struct {
@@ -29,64 +29,73 @@ type task struct {
 func Init() error {
 	Register(visitor, 60*time.Second, "runtime visitor")
 
-	for i := range tasks {
-		t := tasks[i]
-		if t == nil {
-			logger.Task.Warnw("task is nil, skip", "name", t.name, "interval", t.interval.String())
-			continue
-		}
-		if t.interval < time.Second {
-			logger.Task.Warnw("task interval less than 1 second, skip", "name", t.name, "interval", t.interval.String())
-			continue
-		}
-		if t.fn == nil {
-			logger.Task.Warnw("task function is nil, skip", "name", t.name, "interval", t.interval.String())
-			continue
-		}
-		go func() {
-			defer func() {
-				if err := recover(); err != nil {
-					logger.Task.Errorw(fmt.Sprintf("task panic: %s", err), "name", t.name, "interval", t.interval.String())
-				}
-			}()
-			begin := time.Now()
-			logger.Task.Infow("starting task", "name", t.name, "interval", t.interval.String())
-			if err := t.fn(); err != nil {
-				logger.Task.Errorw(fmt.Sprintf("finished task with error: %s", err), "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
-			} else {
-				logger.Task.Infow("finished task", "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
-			}
-
-			ticker := time.NewTicker(t.interval)
-
-			for {
-				select {
-				case <-t.ctx.Done():
-					return
-				case <-ticker.C:
-					begin = time.Now()
-					logger.Task.Infow("starting task", "name", t.name, "interval", t.interval.String())
-					if err := t.fn(); err != nil {
-						logger.Task.Errorw(fmt.Sprintf("finished task with error: %s", err), "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
-						// return
-					} else {
-						logger.Task.Infow("finished task", "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
-						// return
-					}
-				}
-			}
-		}()
-
+	for _, t := range tasks {
+		register(t)
 	}
 
+	inited = true
 	return nil
 }
 
+// Register task can be called at any point before or after Init().
 func Register(fn func() error, interval time.Duration, name string) {
 	mu.Lock()
 	defer mu.Unlock()
+
 	ctx, cancel := context.WithCancel(context.Background())
-	tasks = append(tasks, &task{name: name, fn: fn, interval: interval, ctx: ctx, cancel: cancel})
+	if inited {
+		register(&task{name: name, fn: fn, interval: interval, ctx: ctx, cancel: cancel})
+	} else {
+		tasks = append(tasks, &task{name: name, fn: fn, interval: interval, ctx: ctx, cancel: cancel})
+	}
+}
+
+func register(t *task) {
+	if t == nil {
+		logger.Task.Warnw("task is nil, skip", "name", t.name, "interval", t.interval.String())
+		return
+	}
+	if t.interval < time.Second {
+		logger.Task.Warnw("task interval less than 1 second, skip", "name", t.name, "interval", t.interval.String())
+		return
+	}
+	if t.fn == nil {
+		logger.Task.Warnw("task function is nil, skip", "name", t.name, "interval", t.interval.String())
+		return
+	}
+	go func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Task.Errorw(fmt.Sprintf("task panic: %s", err), "name", t.name, "interval", t.interval.String())
+			}
+		}()
+		begin := time.Now()
+		logger.Task.Infow("starting task", "name", t.name, "interval", t.interval.String())
+		if err := t.fn(); err != nil {
+			logger.Task.Errorw(fmt.Sprintf("finished task with error: %s", err), "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
+		} else {
+			logger.Task.Infow("finished task", "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
+		}
+
+		ticker := time.NewTicker(t.interval)
+
+		for {
+			select {
+			case <-t.ctx.Done():
+				return
+			case <-ticker.C:
+				begin = time.Now()
+				logger.Task.Infow("starting task", "name", t.name, "interval", t.interval.String())
+				if err := t.fn(); err != nil {
+					logger.Task.Errorw(fmt.Sprintf("finished task with error: %s", err), "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
+					// return
+				} else {
+					logger.Task.Infow("finished task", "name", t.name, "interval", t.interval.String(), "cost", time.Since(begin).String())
+					// return
+				}
+			}
+		}
+	}()
 }
 
 func visitor() error {
