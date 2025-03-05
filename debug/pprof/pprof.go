83b3b923ae6003760902bcf426ff4b1c7a57647e
hybfkuf <fredjoseph863@gmail.com>
2025-03-05 17:53:30 +0800

    fix(debug): improve pprof shutdown handing

--- debug/pprof/pprof.go
+++ debug/pprof/pprof.go
@@ -5,20 +5,43 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/forbearing/golib/config"
 	"go.uber.org/zap"
 )
 
 func Run() error {
-	if config.App.EnablePprof {
-		server := &http.Server{
-			Addr:    fmt.Sprintf("%s:%d", config.App.PprofListen, config.App.PprofPort),
-			Handler: http.DefaultServeMux,
-		}
-		zap.S().Infow("successfully start pprof", "listen", config.App.PprofListen, "port", config.App.PprofPort)
-		return server.ListenAndServe()
+	if !config.App.EnablePprof {
+		return nil
 	}
-	<-context.Background().Done()
+
+	log := zap.S()
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", config.App.PprofListen, config.App.PprofPort),
+		Handler: http.DefaultServeMux,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		log.Infow("pprof server started", "listen", config.App.PprofListen, "port", config.App.PprofPort)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Errorw("failed to start pprof", "err", err)
+		}
+	}()
+
+	sig := <-quit
+	log.Infow("pprof shutdown initiated", "signal", sig)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Errorw("pprof shutdown failed", "err", err, "signal", sig)
+		return err
+	}
+	log.Infow("pprof shutdown completed", "signal", sig)
 	return nil
 }
