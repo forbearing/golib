c17f8e69f6ff7ab4e2fd30c273cb79927b9c08b1
hybfkuf <fredjoseph863@gmail.com>
2025-03-05 17:52:57 +0800

    fix(debug): improve statsviz shutdown handing

--- debug/statsviz/statsviz.go
+++ debug/statsviz/statsviz.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/arl/statsviz"
 	"github.com/forbearing/golib/config"
@@ -11,13 +15,36 @@ import (
 )
 
 func Run() error {
-	if config.App.EnableStatsviz {
-		mux := http.NewServeMux()
-		statsviz.Register(mux)
-		zap.S().Infow("successfully start statsviz", "listen", config.App.StatsvizListen, "port", config.App.StatsvizPort)
-		return http.ListenAndServe(fmt.Sprintf("%s:%d", config.App.StatsvizListen, config.App.StatsvizPort), mux)
+	if !config.App.EnableStatsviz {
+		return nil
 	}
 
-	<-context.Background().Done()
+	log := zap.S()
+	mux := http.NewServeMux()
+	statsviz.Register(mux)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%s:%d", config.App.StatsvizListen, config.App.StatsvizPort),
+		Handler: mux,
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		log.Infow("statsviz server started", "listen", config.App.StatsvizListen, "port", config.App.StatsvizPort)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Errorw("failed to start statsviz", "err", err)
+		}
+	}()
+
+	sig := <-quit
+	log.Infow("statsviz shutdown initiated", "signal", sig)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Errorw("statsviz shutdown failed", "err", err, "signal", sig)
+		return err
+	}
+	log.Infow("statsviz shutdown completed", "signal", sig)
 	return nil
 }
