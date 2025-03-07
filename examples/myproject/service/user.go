c0fec700aa8197b626e141806ea15e34f8ba980a
hybfkuf <fredjoseph863@gmail.com>
2025-03-07 15:14:23 +0800

    chore(example): update examples/myproject

--- examples/myproject/service/user.go
+++ examples/myproject/service/user.go
@@ -86,3 +86,51 @@ func (u *user) GetAfter(ctx *types.ServiceContext, users ...*pkgmodel.User) erro
 	log.Info("user get after")
 	return nil
 }
+
+func (u *user) BatchCreateBefore(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_CREATE_BEFORE)
+	log.Info("user batch create before")
+	return nil
+}
+
+func (u *user) BatchCreateAfter(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_CREATE_AFTER)
+	log.Info("user batch create after")
+	return nil
+}
+
+func (u *user) BatchDeleteBefore(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_DELETE_BEFORE)
+	log.Info("user batch delete before")
+	return nil
+}
+
+func (u *user) BatchDeleteAfter(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_DELETE_AFTER)
+	log.Info("user batch delete after")
+	return nil
+}
+
+func (u *user) BatchUpdateBefore(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_UPDATE_BEFORE)
+	log.Info("user batch update before")
+	return nil
+}
+
+func (u *user) BatchUpdateAfter(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_UPDATE_AFTER)
+	log.Info("user batch update after")
+	return nil
+}
+
+func (u *user) BatchUpdatePartialBefore(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_UPDATE_PARTIAL_BEFORE)
+	log.Info("user batch update partial before")
+	return nil
+}
+
+func (u *user) BatchUpdatePartialAfter(ctx *types.ServiceContext, users ...*pkgmodel.User) error {
+	log := u.WithServiceContext(ctx, consts.PHASE_BATCH_UPDATE_PARTIAL_AFTER)
+	log.Info("user batch update partial after")
+	return nil
+}
