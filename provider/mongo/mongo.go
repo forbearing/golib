9b25694e802528efdbac2d0ffd5a3e501ace6c1c
hybfkuf <fredjoseph863@gmail.com>
2025-03-11 15:44:23 +0800

    fix(mongo): prevent potential use of invalid client on connection failure

--- provider/mongo/mongo.go
+++ provider/mongo/mongo.go
@@ -46,6 +46,7 @@ func Init() (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		client = nil
 		return errors.Wrap(err, "failed to connect to mongodb")
 	}
 	zap.S().Infow("successfully connect to mongodb", "host", cfg.Host, "port", cfg.Port, "database", cfg.Database)
