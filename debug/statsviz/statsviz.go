310e1ea1fbf7262c0bb0877408fa4435e3cc14cf
hybfkuf <fredjoseph863@gmail.com>
2025-03-12 16:58:17 +0800

    refactor(statsviz): improve server liftcycle management

--- debug/statsviz/statsviz.go
+++ debug/statsviz/statsviz.go
@@ -4,9 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/arl/statsviz"
@@ -14,37 +11,41 @@ import (
 	"go.uber.org/zap"
 )
 
+var server *http.Server
+
 func Run() error {
 	if !config.App.StatsvizEnable {
 		return nil
 	}
 
-	log := zap.S()
 	mux := http.NewServeMux()
 	statsviz.Register(mux)
-
-	server := &http.Server{
+	server = &http.Server{
 		Addr:    fmt.Sprintf("%s:%d", config.App.StatsvizListen, config.App.StatsvizPort),
 		Handler: mux,
 	}
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		log.Infow("statsviz server started", "listen", config.App.StatsvizListen, "port", config.App.StatsvizPort)
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Errorw("failed to start statsviz", "err", err)
-		}
-	}()
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		zap.S().Errorw("failed to start statsviz server", "err", err)
+		return err
+	}
+	zap.S().Infow("statsviz server started", "listen", config.App.StatsvizListen, "port", config.App.StatsvizPort)
 
-	sig := <-quit
-	log.Infow("statsviz shutdown initiated", "signal", sig)
+	return nil
+}
+
+func Stop() {
+	if server == nil {
+		return
+	}
+
+	zap.S().Infow("statsviz server shutdown initiated")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
-		log.Errorw("statsviz shutdown failed", "err", err, "signal", sig)
-		return err
+		zap.S().Errorw("statsviz server shutdown failed", "err", err)
+	} else {
+		zap.S().Infow("statsviz server shutdown completed")
 	}
-	log.Infow("statsviz shutdown completed", "signal", sig)
-	return nil
+	server = nil
 }
