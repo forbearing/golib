c332157ecf370dcc726bdaac8995e78b805a6b2a
hybfkuf <fredjoseph863@gmail.com>
2025-04-20 22:48:24 +0300

    feat(middleware): add authorization logging

--- middleware/authz.go
+++ middleware/authz.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"github.com/forbearing/golib/logger"
 	"github.com/forbearing/golib/rbac"
 	. "github.com/forbearing/golib/response"
 	"github.com/forbearing/golib/types/consts"
@@ -8,6 +9,7 @@ import (
 	"go.uber.org/zap"
 )
 
+// Authz 中间件开启时必须也开启 JwtAuth 中间件, 要不然 username 会为空则则 isAllow 总是为 false.
 func Authz() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// isAllow, err := rbac.RBAC.Enforcer(c.GetString(consts.CTX_USERNAME), c.Request.URL.Path, c.Request.Method)
@@ -18,6 +20,11 @@ func Authz() gin.HandlerFunc {
 		} else {
 			isAllow, err = rbac.RBAC.Enforcer(c.GetString(consts.CTX_USER_ID), c.Request.URL.Path, c.Request.Method)
 		}
+		logger.Authz.Infoz("", zap.String("username", c.GetString(consts.CTX_USERNAME)),
+			zap.String("path", c.Request.URL.Path),
+			zap.String("method", c.Request.Method),
+			zap.Bool("is_allow", isAllow),
+		)
 		if err != nil {
 			zap.S().Error(err)
 			ResponseJSON(c, CodeFailure)
