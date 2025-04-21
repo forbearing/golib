c662059e9f1d9da85d87669eb56ab792ebc1cfcc
hybfkuf <fredjoseph863@gmail.com>
2025-04-21 09:54:41 +0300

    style(middleware): change logger style

--- middleware/authz.go
+++ middleware/authz.go
@@ -9,7 +9,8 @@ import (
 	"go.uber.org/zap"
 )
 
-// Authz 中间件开启时必须也开启 JwtAuth 中间件, 要不然 username 会为空则则 isAllow 总是为 false.
+// Authz 中间件开启时必须也开启 JwtAuth 中间件, 并且位于 JwtAuth 之后.
+// 要不然 username 会为空则则 isAllow 总是为 false.
 func Authz() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// isAllow, err := rbac.RBAC.Enforcer(c.GetString(consts.CTX_USERNAME), c.Request.URL.Path, c.Request.Method)
@@ -20,7 +21,8 @@ func Authz() gin.HandlerFunc {
 		} else {
 			isAllow, err = basic.RBAC.Enforcer(c.GetString(consts.CTX_USER_ID), c.Request.URL.Path, c.Request.Method)
 		}
-		logger.Authz.Infoz("", zap.String("username", c.GetString(consts.CTX_USERNAME)),
+		logger.Authz.Infoz("",
+			zap.String("username", c.GetString(consts.CTX_USERNAME)),
 			zap.String("path", c.Request.URL.Path),
 			zap.String("method", c.Request.Method),
 			zap.Bool("is_allow", isAllow),
