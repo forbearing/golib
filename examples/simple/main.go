bead66adb95b46609bd9d0ccfa30bcc2b84c2f88
hybfkuf <fredjoseph863@gmail.com>
2025-04-26 08:22:43 +0300

    chore(example): upgrade golib to latest

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -16,9 +16,9 @@ func main() {
 	util.RunOrDie(bootstrap.Bootstrap)
 
 	// Set up your routes here, the routes are not required.
-	router.API.GET("/ping", func(c *gin.Context) { c.String(http.StatusOK, "pong") })
-	router.API.GET("/hello", func(c *gin.Context) { c.String(http.StatusOK, "hello world!") })
-	router.Register[*model.User](router.API, "user")
+	router.API().GET("/ping", func(c *gin.Context) { c.String(http.StatusOK, "pong") })
+	router.API().GET("/hello", func(c *gin.Context) { c.String(http.StatusOK, "hello world!") })
+	router.Register[*model.User](router.API(), "user")
 
 	util.RunOrDie(bootstrap.Run)
 }
