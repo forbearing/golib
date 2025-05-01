4423541e039ce88d9d3f9631cd6d31f505ab8ea2
hybfkuf <fredjoseph863@gmail.com>
2025-05-01 22:09:50 +0300

    feat(cache): add package cache/freecache and implements interface `Cache`

---
+++ cache/freecache/freecache.go
@@ -0,0 +1,85 @@
+package freecache
+
+import (
+	"reflect"
+
+	"github.com/coocood/freecache"
+	"github.com/forbearing/golib/config"
+	"github.com/forbearing/golib/types"
+	jsoniter "github.com/json-iterator/go"
+	cmap "github.com/orcaman/concurrent-map/v2"
+	"go.uber.org/zap"
+)
+
+var (
+	json     = jsoniter.ConfigCompatibleWithStandardLibrary
+	cacheMap = cmap.New[any]()
+)
+
+func Init() error {
+	return nil
+}
+
+type cache[T any] struct {
+	c *freecache.Cache
+}
+
+func Cache[T any]() types.Cache[T] {
+	typ := reflect.TypeOf((*T)(nil)).Elem()
+	key := typ.PkgPath() + "|" + typ.String()
+	val, exists := cacheMap.Get(key)
+	if !exists {
+		val = &cache[T]{c: freecache.NewCache(config.App.Cache.Capacity)}
+		cacheMap.Set(key, val)
+	}
+	return val.(*cache[T])
+}
+
+func (c *cache[T]) Set(key string, value T) {
+	val, err := json.Marshal(value)
+	if err != nil {
+		zap.S().Error(err)
+	} else {
+		if err := c.c.Set([]byte(key), val, int(config.App.Cache.Expiration)); err != nil {
+			zap.S().Error(err)
+		}
+	}
+}
+
+func (c *cache[T]) Get(key string) (T, bool) {
+	var zero T
+	val, err := c.c.Get([]byte(key))
+	if err != nil {
+		// if not found, not log error
+		return zero, false
+	}
+	var result T
+	err = json.Unmarshal(val, &result)
+	if err != nil {
+		zap.S().Error(err)
+		return zero, false
+	}
+	return result, true
+}
+
+func (c *cache[T]) Peek(key string) (T, bool) {
+	return c.Get(key)
+}
+
+func (c *cache[T]) Remove(key string) { c.c.Del([]byte(key)) }
+func (c *cache[T]) Exists(key string) bool {
+	_, err := c.c.Get([]byte(key))
+	return err == nil
+}
+
+func (c *cache[T]) Keys() []string {
+	keys := make([]string, 0)
+	it := c.c.NewIterator()
+	for entry := it.Next(); entry != nil; entry = it.Next() {
+		keys = append(keys, string(entry.Key))
+	}
+	return keys
+}
+
+func (c *cache[T]) Count() int { return int(c.c.EntryCount()) }
+func (c *cache[T]) Flush()     { c.c.Clear() }
