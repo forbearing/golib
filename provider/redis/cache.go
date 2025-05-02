ad77f72fb2505126e1c2bdcb312736f768ac6042
hybfkuf <fredjoseph863@gmail.com>
2025-05-02 10:10:10 +0300

    feat(redis): implement interface `Cache`

---
+++ provider/redis/cache.go
@@ -0,0 +1,118 @@
+package redis
+
+import (
+	"context"
+	"time"
+
+	"github.com/forbearing/golib/types"
+	"go.uber.org/zap"
+)
+
+var _ types.Cache[any] = (*cache[any])(nil)
+
+type cache[T any] struct{}
+
+func Cache[T any]() types.Cache[T] {
+	return new(cache[T])
+}
+
+func (*cache[T]) Set(key string, data T, expiration time.Duration) {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return
+	}
+	val, err := json.Marshal(data)
+	if err != nil {
+		zap.S().Error(err)
+		return
+	}
+	if err := cli.Set(context.Background(), key, val, expiration).Err(); err != nil {
+		zap.S().Error(err)
+	}
+}
+
+func (*cache[T]) Get(key string) (T, bool) {
+	var zero T
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return zero, false
+	}
+	val, err := cli.Get(context.Background(), key).Result()
+	if err != nil {
+		zap.S().Error(err)
+		return zero, false
+	}
+	var result T
+	err = json.Unmarshal([]byte(val), &result)
+	if err != nil {
+		zap.S().Error(err)
+		return zero, false
+	}
+	return result, true
+}
+
+func (c *cache[T]) Peek(key string) (T, bool) {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return *new(T), false
+	}
+	return c.Get(key)
+}
+
+func (*cache[T]) Delete(key string) {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return
+	}
+	if _, err := cli.Del(context.Background(), key).Result(); err != nil {
+		zap.S().Error(err)
+	}
+}
+
+func (*cache[T]) Exists(key string) bool {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return false
+	}
+	res, err := cli.Exists(context.Background(), key).Result()
+	return err == nil && res > 0
+}
+
+func (*cache[T]) Keys() []string {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return []string{}
+	}
+	// WARNING: KEYS * is not recommended in production!
+	// In Redis Cluster, this only works on a single node.
+	keys, err := cli.Keys(context.Background(), "*").Result()
+	if err != nil {
+		zap.S().Error(err)
+		return []string{}
+	}
+	return keys
+}
+
+func (*cache[T]) Len() int {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return 0
+	}
+	// In Redis Cluster, this only counts the selected node.
+	count, err := cli.DBSize(context.Background()).Result()
+	if err != nil {
+		zap.S().Error(err)
+		return 0
+	}
+	return int(count)
+}
+
+func (*cache[T]) Flush() {
+	if !initialized {
+		zap.S().Warn("redis not initialized")
+		return
+	}
+	if _, err := cli.FlushAll(context.Background()).Result(); err != nil {
+		zap.S().Error(err)
+	}
+}
