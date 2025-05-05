b86d371e8f32fafa97165d2b68232f1ac87e5d44
hybfkuf <fredjoseph863@gmail.com>
2025-05-05 15:38:25 +0800

    test(cache): split test case

--- cache/cache_benchmark_test.go
+++ cache/cache_benchmark_test.go
@@ -27,32 +27,240 @@ type User struct {
 	model.Base
 }
 
-func benchInt(b *testing.B, cache types.Cache[int]) {
-	b.Run("Get", func(b *testing.B) {
-		for i := range b.N {
-			cache.Set(fmt.Sprintf("key%d", i), i, config.App.Cache.Expiration)
-		}
-		b.ResetTimer()
-		for i := range b.N {
-			cache.Get(fmt.Sprintf("key%d", i))
-		}
+func init() {
+	os.Setenv(config.REDIS_ENABLE, "true")
+	os.Setenv(config.MEMCACHED_ENABLE, "true")
+	os.Setenv(config.REDIS_ADDR, "127.0.0.1:6378")
+	os.Setenv(config.REDIS_PASSWORD, "password123")
+	if err := config.Init(); err != nil {
+		panic(err)
+	}
+	if err := zap.Init(); err != nil {
+		panic(err)
+	}
+	if err := redis.Init(); err != nil {
+		panic(err)
+	}
+	if err := memcached.Init(); err != nil {
+		panic(err)
+	}
+	if err := cache.Init(); err != nil {
+		panic(err)
+	}
+}
+
+func BenchmarkInt(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchInt(b, cache.Cache[int]())
 	})
+	b.Run("lru", func(b *testing.B) {
+		benchInt(b, lru.Cache[int]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchInt(b, lrue.Cache[int]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchInt(b, cmap.Cache[int]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchInt(b, smap.Cache[int]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchInt(b, bigcache.Cache[int]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchInt(b, freecache.Cache[int]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchInt(b, fastcache.Cache[int]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchInt(b, redis.Cache[int]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchInt(b, memcached.Cache[int]())
+	})
+}
+
+func BenchmarkIntParallel(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchIntParallel(b, cache.Cache[int]())
+	})
+	b.Run("lru", func(b *testing.B) {
+		benchIntParallel(b, lru.Cache[int]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchIntParallel(b, lrue.Cache[int]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchIntParallel(b, cmap.Cache[int]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchIntParallel(b, smap.Cache[int]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchIntParallel(b, bigcache.Cache[int]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchIntParallel(b, freecache.Cache[int]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchIntParallel(b, fastcache.Cache[int]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchIntParallel(b, redis.Cache[int]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchIntParallel(b, memcached.Cache[int]())
+	})
+}
+
+func BenchmarkString(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchString(b, cache.Cache[string]())
+	})
+	b.Run("lru", func(b *testing.B) {
+		benchString(b, lru.Cache[string]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchString(b, lrue.Cache[string]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchString(b, cmap.Cache[string]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchString(b, smap.Cache[string]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchString(b, bigcache.Cache[string]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchString(b, freecache.Cache[string]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchString(b, fastcache.Cache[string]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchString(b, redis.Cache[string]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchString(b, memcached.Cache[string]())
+	})
+}
+
+func BenchmarkStringParallel(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchStringParallel(b, cache.Cache[string]())
+	})
+	b.Run("lru", func(b *testing.B) {
+		benchStringParallel(b, lru.Cache[string]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchStringParallel(b, lrue.Cache[string]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchStringParallel(b, cmap.Cache[string]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchStringParallel(b, smap.Cache[string]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchStringParallel(b, bigcache.Cache[string]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchStringParallel(b, freecache.Cache[string]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchStringParallel(b, fastcache.Cache[string]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchStringParallel(b, redis.Cache[string]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchStringParallel(b, memcached.Cache[string]())
+	})
+}
+
+func BenchmarkUser(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchUser(b, cache.Cache[User]())
+	})
+	b.Run("lru", func(b *testing.B) {
+		benchUser(b, lru.Cache[User]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchUser(b, lrue.Cache[User]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchUser(b, cmap.Cache[User]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchUser(b, smap.Cache[User]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchUser(b, bigcache.Cache[User]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchUser(b, freecache.Cache[User]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchUser(b, fastcache.Cache[User]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchUser(b, redis.Cache[User]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchUser(b, memcached.Cache[User]())
+	})
+}
+
+func BenchmarkUserParallel(b *testing.B) {
+	b.Run("cache", func(b *testing.B) {
+		benchUserParallel(b, cache.Cache[User]())
+	})
+	b.Run("lru", func(b *testing.B) {
+		benchUserParallel(b, lru.Cache[User]())
+	})
+	b.Run("lrue", func(b *testing.B) {
+		benchUserParallel(b, lrue.Cache[User]())
+	})
+	b.Run("cmap", func(b *testing.B) {
+		benchUserParallel(b, cmap.Cache[User]())
+	})
+	b.Run("smap", func(b *testing.B) {
+		benchUserParallel(b, smap.Cache[User]())
+	})
+	b.Run("bigcache", func(b *testing.B) {
+		benchUserParallel(b, bigcache.Cache[User]())
+	})
+	b.Run("freecache", func(b *testing.B) {
+		benchUserParallel(b, freecache.Cache[User]())
+	})
+	b.Run("fastcache", func(b *testing.B) {
+		benchUserParallel(b, fastcache.Cache[User]())
+	})
+	b.Run("redis", func(b *testing.B) {
+		benchUserParallel(b, redis.Cache[User]())
+	})
+	b.Run("memcached", func(b *testing.B) {
+		benchUserParallel(b, memcached.Cache[User]())
+	})
+}
+
+func benchInt(b *testing.B, cache types.Cache[int]) {
 	b.Run("Set", func(b *testing.B) {
 		for i := range b.N {
 			cache.Set(fmt.Sprintf("key%d", i), i, config.App.Cache.Expiration)
 		}
 	})
-
-	b.Run("Get Parallel", func(b *testing.B) {
-		b.RunParallel(func(p *testing.PB) {
-			i := 0
-			for p.Next() {
-				cache.Get(fmt.Sprintf("key%d", i))
-				i++
-			}
-		})
+	b.Run("Get", func(b *testing.B) {
+		for i := range b.N {
+			_, _ = cache.Get(fmt.Sprintf("key%d", i))
+		}
 	})
+}
 
+func benchIntParallel(b *testing.B, cache types.Cache[int]) {
 	b.Run("Set Parallel", func(b *testing.B) {
 		b.RunParallel(func(p *testing.PB) {
 			i := 0
@@ -62,32 +270,31 @@ func benchInt(b *testing.B, cache types.Cache[int]) {
 			}
 		})
 	})
+	b.Run("Get Parallel", func(b *testing.B) {
+		b.RunParallel(func(p *testing.PB) {
+			i := 0
+			for p.Next() {
+				_, _ = cache.Get(fmt.Sprintf("key%d", i))
+				i++
+			}
+		})
+	})
 }
 
 func benchString(b *testing.B, cache types.Cache[string]) {
-	b.Run("Get", func(b *testing.B) {
-		for i := range b.N {
-			cache.Set(fmt.Sprintf("key%d", i), strconv.Itoa(i), config.App.Cache.Expiration)
-		}
-		b.ResetTimer()
-		for i := range b.N {
-			cache.Get(fmt.Sprintf("key%d", i))
-		}
-	})
 	b.Run("Set", func(b *testing.B) {
 		for i := range b.N {
 			cache.Set(fmt.Sprintf("key%d", i), strconv.Itoa(i), config.App.Cache.Expiration)
 		}
 	})
-	b.Run("Get Parallel", func(b *testing.B) {
-		b.RunParallel(func(p *testing.PB) {
-			i := 0
-			for p.Next() {
-				cache.Get(fmt.Sprintf("key%d", i))
-				i++
-			}
-		})
+	b.Run("Get", func(b *testing.B) {
+		for i := range b.N {
+			_, _ = cache.Get(fmt.Sprintf("key%d", i))
+		}
 	})
+}
+
+func benchStringParallel(b *testing.B, cache types.Cache[string]) {
 	b.Run("Set Parallel", func(b *testing.B) {
 		b.RunParallel(func(p *testing.PB) {
 			i := 0
@@ -97,32 +304,31 @@ func benchString(b *testing.B, cache types.Cache[string]) {
 			}
 		})
 	})
+	b.Run("Get Parallel", func(b *testing.B) {
+		b.RunParallel(func(p *testing.PB) {
+			i := 0
+			for p.Next() {
+				_, _ = cache.Get(fmt.Sprintf("key%d", i))
+				i++
+			}
+		})
+	})
 }
 
 func benchUser(b *testing.B, cache types.Cache[User]) {
-	b.Run("Get", func(b *testing.B) {
-		for i := range b.N {
-			cache.Set(fmt.Sprintf("key%d", i), User{Name: fmt.Sprintf("user%d", i)}, config.App.Cache.Expiration)
-		}
-		b.ResetTimer()
-		for i := range b.N {
-			cache.Get(fmt.Sprintf("key%d", i))
-		}
-	})
 	b.Run("Set", func(b *testing.B) {
 		for i := range b.N {
 			cache.Set(fmt.Sprintf("key%d", i), User{Name: fmt.Sprintf("user%d", i)}, config.App.Cache.Expiration)
 		}
 	})
-	b.Run("Get Parallel", func(b *testing.B) {
-		b.RunParallel(func(p *testing.PB) {
-			i := 0
-			for p.Next() {
-				cache.Get(fmt.Sprintf("key%d", i))
-				i++
-			}
-		})
+	b.Run("Get", func(b *testing.B) {
+		for i := range b.N {
+			_, _ = cache.Get(fmt.Sprintf("key%d", i))
+		}
 	})
+}
+
+func benchUserParallel(b *testing.B, cache types.Cache[User]) {
 	b.Run("Set Parallel", func(b *testing.B) {
 		b.RunParallel(func(p *testing.PB) {
 			i := 0
@@ -132,123 +338,13 @@ func benchUser(b *testing.B, cache types.Cache[User]) {
 			}
 		})
 	})
-}
-
-func BenchmarkCache(b *testing.B) {
-	os.Setenv(config.REDIS_ENABLE, "true")
-	os.Setenv(config.MEMCACHED_ENABLE, "true")
-	os.Setenv(config.REDIS_ADDR, "127.0.0.1:6378")
-	os.Setenv(config.REDIS_PASSWORD, "password123")
-	if err := config.Init(); err != nil {
-		b.Fatal(err)
-	}
-	if err := zap.Init(); err != nil {
-		b.Fatal(err)
-	}
-	if err := redis.Init(); err != nil {
-		b.Fatal(err)
-	}
-	if err := memcached.Init(); err != nil {
-		b.Fatal(err)
-	}
-	if err := cache.Init(); err != nil {
-		b.Fatal(err)
-	}
-
-	b.Run("int", func(b *testing.B) {
-		b.Run("cache", func(b *testing.B) {
-			benchInt(b, cache.Cache[int]())
-		})
-		b.Run("lru", func(b *testing.B) {
-			benchInt(b, lru.Cache[int]())
-		})
-		b.Run("lrue", func(b *testing.B) {
-			benchInt(b, lrue.Cache[int]())
-		})
-		b.Run("cmap", func(b *testing.B) {
-			benchInt(b, cmap.Cache[int]())
-		})
-		b.Run("smap", func(b *testing.B) {
-			benchInt(b, smap.Cache[int]())
-		})
-		b.Run("bigcache", func(b *testing.B) {
-			benchInt(b, bigcache.Cache[int]())
-		})
-		b.Run("freecache", func(b *testing.B) {
-			benchInt(b, freecache.Cache[int]())
-		})
-		b.Run("fastcache", func(b *testing.B) {
-			benchInt(b, fastcache.Cache[int]())
-		})
-		b.Run("redis", func(b *testing.B) {
-			benchInt(b, redis.Cache[int]())
-		})
-		b.Run("memcached", func(b *testing.B) {
-			benchInt(b, memcached.Cache[int]())
-		})
-	})
-	b.Run("string", func(b *testing.B) {
-		b.Run("cache", func(b *testing.B) {
-			benchString(b, cache.Cache[string]())
-		})
-		b.Run("lru", func(b *testing.B) {
-			benchString(b, lru.Cache[string]())
-		})
-		b.Run("lrue", func(b *testing.B) {
-			benchString(b, lrue.Cache[string]())
-		})
-		b.Run("cmap", func(b *testing.B) {
-			benchString(b, cmap.Cache[string]())
-		})
-		b.Run("smap", func(b *testing.B) {
-			benchString(b, smap.Cache[string]())
-		})
-		b.Run("bigcache", func(b *testing.B) {
-			benchString(b, bigcache.Cache[string]())
-		})
-		b.Run("freecache", func(b *testing.B) {
-			benchString(b, freecache.Cache[string]())
-		})
-		b.Run("fastcache", func(b *testing.B) {
-			benchString(b, fastcache.Cache[string]())
-		})
-		b.Run("redis", func(b *testing.B) {
-			benchString(b, redis.Cache[string]())
-		})
-		b.Run("memcached", func(b *testing.B) {
-			benchString(b, memcached.Cache[string]())
-		})
-	})
-	b.Run("user", func(b *testing.B) {
-		b.Run("cache", func(b *testing.B) {
-			benchUser(b, cache.Cache[User]())
-		})
-		b.Run("lru", func(b *testing.B) {
-			benchUser(b, lru.Cache[User]())
-		})
-		b.Run("lrue", func(b *testing.B) {
-			benchUser(b, lrue.Cache[User]())
-		})
-		b.Run("cmap", func(b *testing.B) {
-			benchUser(b, cmap.Cache[User]())
-		})
-		b.Run("smap", func(b *testing.B) {
-			benchUser(b, smap.Cache[User]())
-		})
-		b.Run("bigcache", func(b *testing.B) {
-			benchUser(b, bigcache.Cache[User]())
-		})
-		b.Run("freecache", func(b *testing.B) {
-			benchUser(b, freecache.Cache[User]())
-		})
-		b.Run("fastcache", func(b *testing.B) {
-			benchUser(b, fastcache.Cache[User]())
-		})
-		b.Run("redis", func(b *testing.B) {
-			benchUser(b, redis.Cache[User]())
-		})
-		b.Run("memcached", func(b *testing.B) {
-			benchUser(b, memcached.Cache[User]())
+	b.Run("Get Parallel", func(b *testing.B) {
+		b.RunParallel(func(p *testing.PB) {
+			i := 0
+			for p.Next() {
+				_, _ = cache.Get(fmt.Sprintf("key%d", i))
+				i++
+			}
 		})
 	})
 }
