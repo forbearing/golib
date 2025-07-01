abf948ddf10437ad871e7cb31fab47d1f0a6bb9d
hybfkuf <fredjoseph863@gmail.com>
2025-07-01 22:24:34 +0800

    chore(example): update examples/simple

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/forbearing/golib/bootstrap"
 	"github.com/forbearing/golib/router"
+	"github.com/forbearing/golib/types/consts"
 	"github.com/forbearing/golib/util"
 	"github.com/gin-gonic/gin"
 )
@@ -18,7 +19,7 @@ func main() {
 	// Set up your routes here, the routes are not required.
 	router.API().GET("/ping", func(c *gin.Context) { c.String(http.StatusOK, "pong") })
 	router.API().GET("/hello", func(c *gin.Context) { c.String(http.StatusOK, "hello world!") })
-	router.Register[*model.User](router.API(), "user")
+	router.Register[*model.User](router.API(), "user", consts.Most)
 
 	util.RunOrDie(bootstrap.Run)
 }
