88a80633b0472dbb715a32c83c03dab0818398c3
hybfkuf <fredjoseph863@gmail.com>
2025-07-29 23:29:49 +0300

    feat(dsl): prepare support dsl

---
+++ dsl/dsl.go
@@ -0,0 +1,15 @@
+package dsl
+
+func Create(func())             {}
+func Delete(func())             {}
+func Update(func())             {}
+func UpdatePartial(func())      {}
+func Get(func())                {}
+func List(func())               {}
+func BatchCreate(func())        {}
+func BatchDelete(func())        {}
+func BatchUpdate(func())        {}
+func BatchUpdatePartial(func()) {}
+
+func Payload(any) {}
+func Result(any)  {}
