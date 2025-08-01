5bf014736016fd08cb15781ba0ad3fda5c62a034
hybfkuf <fredjoseph863@gmail.com>
2025-08-01 07:51:56 +0300

    feat(dsl): dsl parser

--- dsl/parser.go
+++ dsl/parser.go
@@ -1,11 +1,8 @@
 package dsl
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
-
-	"github.com/kr/pretty"
 )
 
 func Parse(stmts []ast.Stmt) *Design {
@@ -46,34 +43,67 @@ func Parse(stmts []ast.Stmt) *Design {
 			}
 		}
 
-		// Parse "Create" design.
-		if ident.Name == "Create" && len(call.Args) == 1 {
-			if flit, ok := call.Args[0].(*ast.FuncLit); ok && flit != nil && flit.Body != nil {
-				// Payload or Result.
-				for _, stmt := range flit.Body.List {
-					if expr, ok := stmt.(*ast.ExprStmt); ok && expr != nil {
-						if call, ok := expr.X.(*ast.CallExpr); ok && call != nil && call.Fun != nil {
-							if identExpr, ok := call.Fun.(*ast.IndexExpr); ok && identExpr != nil {
-								var isPayload bool
-								var isResult bool
-								_ = isResult
-								if ident, ok := identExpr.X.(*ast.Ident); ok && ident != nil {
-									if ident.Name == "Payload" {
-										isPayload = true
-									} else if ident.Name == "Result" {
-										isResult = true
+		if payload, result, exists := parseAction("Create", ident, call.Args); exists {
+			design.Create = Action{Payload: payload, Result: result}
+		}
+		if payload, result, exists := parseAction("Update", ident, call.Args); exists {
+			design.Update = Action{Payload: payload, Result: result}
+		}
+		if payload, result, exists := parseAction("UpdatePartial", ident, call.Args); exists {
+			design.UpdatePartial = Action{Payload: payload, Result: result}
+		}
+		if payload, result, exists := parseAction("BatchCreate", ident, call.Args); exists {
+			design.BatchCreate = Action{Payload: payload, Result: result}
+		}
+		if payload, result, exists := parseAction("BatchUpdate", ident, call.Args); exists {
+			design.BatchUpdate = Action{Payload: payload, Result: result}
+		}
+		if payload, result, exists := parseAction("BatchUpdatePartial", ident, call.Args); exists {
+			design.BatchUpdatePartial = Action{Payload: payload, Result: result}
+		}
+
+	}
+
+	return design
+}
+
+func parseAction(name string, ident *ast.Ident, args []ast.Expr) (string, string, bool) {
+	var payload string
+	var result string
+
+	if ident.Name == name && len(args) == 1 {
+		if flit, ok := args[0].(*ast.FuncLit); ok && flit != nil && flit.Body != nil {
+			// Payload or Result.
+			for _, stmt := range flit.Body.List {
+				if expr, ok := stmt.(*ast.ExprStmt); ok && expr != nil {
+					if call, ok := expr.X.(*ast.CallExpr); ok && call != nil && call.Fun != nil {
+						if identExpr, ok := call.Fun.(*ast.IndexExpr); ok && identExpr != nil {
+							var isPayload bool
+							var isResult bool
+							_ = isResult
+							if ident, ok := identExpr.X.(*ast.Ident); ok && ident != nil {
+								switch ident.Name {
+								case "Payload":
+									isPayload = true
+								case "Result":
+									isResult = true
+								}
+							}
+							if isPayload {
+								if ident, ok := identExpr.Index.(*ast.Ident); ok && ident != nil { // Payload[User]
+									payload = ident.Name
+								} else if starExpr, ok := identExpr.Index.(*ast.StarExpr); ok && starExpr != nil { // Payload[*User]
+									if ident, ok := starExpr.X.(*ast.Ident); ok && ident != nil {
+										payload = ident.Name
 									}
 								}
-								if isPayload {
-									// Payload[User]
-									if ident, ok := identExpr.Index.(*ast.Ident); ok && ident != nil {
-										fmt.Println("----- Payload", ident.Name)
-									}
-									// Payload[*User]
-									if starExpr, ok := identExpr.Index.(*ast.StarExpr); ok && starExpr != nil {
-										if ident, ok := starExpr.X.(*ast.Ident); ok && ident != nil {
-											fmt.Println("----- Payload", ident.Name)
-										}
+							}
+							if isResult {
+								if ident, ok := identExpr.Index.(*ast.Ident); ok && ident != nil { // Result[User]
+									result = ident.Name
+								} else if starExpr, ok := identExpr.Index.(*ast.StarExpr); ok && starExpr != nil { // Result[*User]
+									if ident, ok := starExpr.X.(*ast.Ident); ok && ident != nil {
+										result = ident.Name
 									}
 								}
 							}
@@ -82,9 +112,8 @@ func Parse(stmts []ast.Stmt) *Design {
 				}
 			}
 		}
-
+		return payload, result, true
 	}
-	pretty.Println(design)
 
-	return design
+	return "", "", false
 }
