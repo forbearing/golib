6405f59d490a368dd00b6e285141014975f51ca5
hybfkuf <fredjoseph863@gmail.com>
2025-08-10 21:03:36 +0300

    test(codegen): enhance ApplyServiceFile tests to assert formatted output

--- internal/codegen/gen/testdata/service/user_create.go
+++ internal/codegen/gen/testdata/service/user_create.go
@@ -6,10 +6,10 @@ import (
 	"github.com/forbearing/golib/types"
 )
 
-func (u *user) Create(ctx *types.ServiceContext, req *model.UserReq) (*model.UserRsp, error) {
+func (u *user) Create(ctx *types.ServiceContext, req *model.User) (rsp *model.User, err error) {
 	log := u.WithServiceContext(ctx, ctx.GetPhase())
 	log.Info("user create")
-	return &model.UserRsp{}, nil
+	return rsp, nil
 }
 
 func (u *user) CreateBefore(ctx *types.ServiceContext, user *model.User) error {
