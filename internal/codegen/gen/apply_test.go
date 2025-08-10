27f5328de2691f061ef5abaff1e01db998087aaa
hybfkuf <fredjoseph863@gmail.com>
2025-08-10 21:03:36 +0300

    test(codegen): enhance ApplyServiceFile tests to assert formatted output

--- internal/codegen/gen/apply_test.go
+++ internal/codegen/gen/apply_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/forbearing/golib/dsl"
 	"github.com/forbearing/golib/internal/codegen/gen"
+	"github.com/kr/pretty"
 )
 
 func TestApplyServiceFile(t *testing.T) {
@@ -15,15 +16,77 @@ func TestApplyServiceFile(t *testing.T) {
 		// Named input parameters for target function.
 		code   string
 		action *dsl.Action
+		want   string
 	}{
 		{
-			name: "user_create",
+			name: "user_create_with_payload_result",
+			code: dataServiceUserCreate,
+			action: &dsl.Action{
+				Enabled: true,
+				Payload: "UserReq",
+				Result:  "UserRsp",
+			},
+			want: `package service
+
+import (
+	"helloworld/model"
+
+	"github.com/forbearing/golib/types"
+)
+
+func (u *user) Create(ctx *types.ServiceContext, req *model.UserReq) (rsp *model.UserRsp, err error) {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create")
+	return rsp, nil
+}
+
+func (u *user) CreateBefore(ctx *types.ServiceContext, user *model.User) error {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create before")
+	return nil
+}
+
+func (u *user) CreateAfter(ctx *types.ServiceContext, user *model.User) error {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create after")
+	return nil
+}
+`,
+		},
+		{
+			name: "user_create_no_payload_result",
 			code: dataServiceUserCreate,
 			action: &dsl.Action{
 				Enabled: true,
 				Payload: "User",
 				Result:  "User",
 			},
+			want: `package service
+
+import (
+	"helloworld/model"
+
+	"github.com/forbearing/golib/types"
+)
+
+func (u *user) Create(ctx *types.ServiceContext, req *model.User) (rsp *model.User, err error) {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create")
+	return rsp, nil
+}
+
+func (u *user) CreateBefore(ctx *types.ServiceContext, user *model.User) error {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create before")
+	return nil
+}
+
+func (u *user) CreateAfter(ctx *types.ServiceContext, user *model.User) error {
+	log := u.WithServiceContext(ctx, ctx.GetPhase())
+	log.Info("user create after")
+	return nil
+}
+`,
 		},
 	}
 	for _, tt := range tests {
@@ -35,6 +98,14 @@ func TestApplyServiceFile(t *testing.T) {
 				return
 			}
 			gen.ApplyServiceFile(file, tt.action)
+			got, err := gen.FormatNodeExtra(file)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("got:\n%s\nwant:\n%s", pretty.Sprintf("% #v", got), pretty.Sprintf("% #v", tt.want))
+			}
 		})
 	}
 }
