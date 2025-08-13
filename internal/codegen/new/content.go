3a99aa767d428b8f7a21464b19aecb6b06512c71
hybfkuf <fredjoseph863@gmail.com>
2025-08-13 20:35:00 +0800

    feat(codegen/new): new project will includes configx, cronjobx

---
+++ internal/codegen/new/content.go
@@ -0,0 +1,97 @@
+package new
+
+const mainContent = `package main
+
+import (
+	"%s/configx"
+	"%s/cronjobx"
+	"%s/router"
+	"%s/service"
+
+	"github.com/forbearing/golib/bootstrap"
+	. "github.com/forbearing/golib/util"
+)
+
+func main() {
+	RunOrDie(bootstrap.Bootstrap)
+	RunOrDie(configx.Init)
+	RunOrDie(cronjobx.Init)
+	RunOrDie(service.Init)
+	RunOrDie(router.Init)
+	RunOrDie(bootstrap.Run)
+}
+`
+
+const configxContent = `package configx
+
+func Init() error {
+	return nil
+}
+`
+
+const cronjobxContent = `package cronjobx
+
+func Init() error {
+	return nil
+}
+`
+
+const serviceContent = `package service
+
+func Init() error {
+	return nil
+}
+`
+
+const routerContent = `package router
+
+func Init() error {
+	return nil
+}
+`
+
+const gitignoreContent = `# Binaries for programs and plugins
+*.exe
+*.exe~
+*.dll
+*.so
+*.dylib
+
+# Test binary, built with 'go test -c'
+*.test
+
+# Output of the go coverage tool, specifically when used with LiteIDE
+*.out
+
+# Dependency directories (remove the comment below to include it)
+# vendor/
+
+# Go workspace file
+go.work
+
+# IDE files
+.vscode/
+.idea/
+*.swp
+*.swo
+*~
+
+# OS generated files
+.DS_Store
+.DS_Store?
+._*
+.Spotlight-V100
+.Trashes
+ehthumbs.db
+Thumbs.db
+
+# Log files
+*.log
+
+# Temporary files
+tmp/
+temp/
+
+# Build output
+dist/
+build/`
