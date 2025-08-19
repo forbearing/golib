8087702464d1ae7839c1e099cd1873ab7d83d466
hybfkuf <fredjoseph863@gmail.com>
2025-08-19 21:28:41 +0300

    feat(dsl):  add support for `model.Empty` marker structs

--- dsl/parser.go
+++ dsl/parser.go
@@ -15,10 +15,32 @@ import (
 //
 // Pass the "Endpoint" to overwrite the default endpoint.
 func Parse(file *ast.File, endpoint string) map[string]*Design {
+	designBase, designEmpty := parse(file)
+
+	// If struct contains model.Base and model.Empty, then remove it from designEmpty.
+	// model.Base has more priority than model.Empty.
+	for name := range designBase {
+		delete(designEmpty, name)
+	}
+
 	m := make(map[string]*Design)
-	designs := parse(file)
-	for name, fnDecl := range designs {
+	for name, fnDecl := range designBase {
 		design := parseDesign(fnDecl)
+		m[name] = design
+	}
+	for name, fnDecl := range designEmpty {
+		design := parseDesign(fnDecl)
+		// the struct has field model.Empty always should be not migrated.
+		design.Migrate = false
+		m[name] = design
+	}
+
+	// set the default values for Design
+	// Default action, the action always not nil,
+	// Action default value:
+	//	Enabled default to "true"
+	//	Service default to "false"
+	for name, design := range m {
 		// Default endpoint is the lower case of the model name.
 		if len(design.Endpoint) == 0 {
 			design.Endpoint = strings.ToLower(name)
@@ -100,18 +122,23 @@ func initDefaultAction(modelName string, action *Action) {
 // parse will parse the whole file node to find all models with its "Design" method node.
 //
 // key is model name, value is ast node that represents the model "Design" method.
-func parse(file *ast.File) map[string]*ast.FuncDecl {
-	designs := make(map[string]*ast.FuncDecl)
+func parse(file *ast.File) (map[string]*ast.FuncDecl, map[string]*ast.FuncDecl) {
+	designBase := make(map[string]*ast.FuncDecl)
+	designEmpty := make(map[string]*ast.FuncDecl)
 	if file == nil {
-		return designs
+		return designBase, designEmpty
 	}
 
-	models := findAllModelNames(file)
+	modelBase := findAllModelBase(file)
+	modelEmpty := findAllModelEmpty(file)
 	// Every model should always has a *ast.FuncDecl,
 	// If model has no "Design" method, then the value is nil.
 	// It's convenient to generate a default design for the model.
-	for _, model := range models {
-		designs[model] = nil
+	for _, model := range modelBase {
+		designBase[model] = nil
+	}
+	for _, model := range modelEmpty {
+		designEmpty[model] = nil
 	}
 
 	for _, decl := range file.Decls {
@@ -139,15 +166,16 @@ func parse(file *ast.File) map[string]*ast.FuncDecl {
 					recvName = ident.Name
 				}
 			}
-			if slices.Contains(models, recvName) {
-				designs[recvName] = fn
-			} else {
-				designs[recvName] = nil
+			if slices.Contains(modelBase, recvName) {
+				designBase[recvName] = fn
+			}
+			if slices.Contains(modelEmpty, recvName) {
+				designEmpty[recvName] = fn
 			}
 		}
 	}
 
-	return designs
+	return designBase, designEmpty
 }
 
 // parseDesign parse the *ast.FuncDecl that represents "Design" method and returns a *Design object.
@@ -438,8 +466,8 @@ type actionResult struct {
 	resultHasStar  bool
 }
 
-// findAllModelNames finds all model names in the ast File Node.
-func findAllModelNames(file *ast.File) []string {
+// findAllModelBase finds all struct anynomous field that has "model.Base" or "aliasmodelname.Base"
+func findAllModelBase(file *ast.File) []string {
 	names := make([]string, 0)
 	if file == nil {
 		return names
@@ -460,6 +488,38 @@ func findAllModelNames(file *ast.File) []string {
 			}
 			for _, field := range structType.Fields.List {
 				if isModelBase(file, field) {
+					names = append(names, typeSpec.Name.Name)
+					break
+				}
+			}
+		}
+	}
+
+	return names
+}
+
+// findAllModelEmpty finds all struct anynomous field that has "model.Empty" or "aliasmodelname.Empty"
+func findAllModelEmpty(file *ast.File) []string {
+	names := make([]string, 0)
+	if file == nil {
+		return names
+	}
+	for _, decl := range file.Decls {
+		genDecl, ok := decl.(*ast.GenDecl)
+		if !ok || genDecl == nil || genDecl.Tok != token.TYPE {
+			continue
+		}
+		for _, spec := range genDecl.Specs {
+			typeSpec, ok := spec.(*ast.TypeSpec)
+			if !ok || typeSpec == nil {
+				continue
+			}
+			structType, ok := typeSpec.Type.(*ast.StructType)
+			if !ok || structType == nil || structType.Fields == nil {
+				continue
+			}
+			for _, field := range structType.Fields.List {
+				if isModelEmpty(file, field) {
 					names = append(names, typeSpec.Name.Name)
 					break
 				}
@@ -505,6 +565,46 @@ func isModelBase(file *ast.File, field *ast.Field) bool {
 		}
 	case *ast.Ident:
 		return t.Name == "Base"
+	}
+
+	return false
+}
+
+// isModelEmpty check if the struct field is "model.Empty" or "aliasmodelname.Empty"
+//
+/*
+import (
+	"github.com/forbearing/golib/dsl"
+	. "github.com/forbearing/golib/dsl"
+	"github.com/forbearing/golib/model"
+	pkgmodel "github.com/forbearing/golib/model"
+)
+*/
+func isModelEmpty(file *ast.File, field *ast.Field) bool {
+	// Not anonymouse field.
+	if file == nil || field == nil || len(field.Names) != 0 {
+		return false
+	}
+
+	aliasNames := []string{"model"}
+	for _, imp := range file.Imports {
+		if imp.Path == nil {
+			continue
+		}
+		if imp.Path.Value == consts.IMPORT_PATH_MODEL {
+			if imp.Name != nil && !slices.Contains(aliasNames, imp.Name.Name) {
+				aliasNames = append(aliasNames, imp.Name.Name)
+			}
+		}
+	}
+
+	switch t := field.Type.(type) {
+	case *ast.SelectorExpr:
+		if ident, ok := t.X.(*ast.Ident); ok {
+			return slices.Contains(aliasNames, ident.Name) && t.Sel.Name == "Empty"
+		}
+	case *ast.Ident:
+		return t.Name == "Empty"
 	}
 
 	return false
