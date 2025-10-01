3bd73c5b184b081c7d71f87ccd2a3f8c5e2d91ba
hybfkuf <condryhebenstreit@gmail.com>
2025-10-01 19:29:39 +0800

    refactor(cmd/gg): set default directories for `model`,`service`,`router`,`dao` and disable related flags

--- cmd/gg/root.go
+++ cmd/gg/root.go
@@ -3,10 +3,10 @@ package main
 import "github.com/spf13/cobra"
 
 var (
-	modelDir   string
-	serviceDir string
-	routerDir  string
-	daoDir     string
+	modelDir   string = "model"
+	serviceDir string = "service"
+	routerDir  string = "router"
+	daoDir     string = "dao"
 	excludes   []string
 	module     string
 	debug      bool
@@ -21,12 +21,12 @@ var rootCmd = &cobra.Command{
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&module, "module", "M", "", "go project module name")
-	rootCmd.PersistentFlags().StringVarP(&modelDir, "model", "m", "model", "model directory path")
-	rootCmd.PersistentFlags().StringVarP(&serviceDir, "service", "s", "service", "service directory path")
-	rootCmd.PersistentFlags().StringVarP(&routerDir, "router", "r", "router", "router directory path")
-	rootCmd.PersistentFlags().StringVarP(&daoDir, "dao", "", "dao", "dao directory path")
-	rootCmd.PersistentFlags().StringSliceVarP(&excludes, "exclude", "e", nil, "exclude files or directories")
+	// rootCmd.PersistentFlags().StringVarP(&module, "module", "M", "", "go project module name")
+	// rootCmd.PersistentFlags().StringVarP(&modelDir, "model", "m", "model", "model directory path")
+	// rootCmd.PersistentFlags().StringVarP(&serviceDir, "service", "s", "service", "service directory path")
+	// rootCmd.PersistentFlags().StringVarP(&routerDir, "router", "r", "router", "router directory path")
+	// rootCmd.PersistentFlags().StringVarP(&daoDir, "dao", "", "dao", "dao directory path")
+	// rootCmd.PersistentFlags().StringSliceVarP(&excludes, "exclude", "e", nil, "exclude files or directories")
 	rootCmd.PersistentFlags().BoolVarP(&debug, "debug", "d", false, "enable debug logging")
 	rootCmd.PersistentFlags().BoolVar(&prune, "prune", false, "Prune disabled service action files with user confirmation")
 
