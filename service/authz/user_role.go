9917f030d9237e116726f454253b539587a9d158
hybfkuf <condryhebenstreit@gmail.com>
2025-10-10 16:07:45 +0800

    refactor(model): rename model 2

--- service/authz/user_role.go
+++ service/authz/user_role.go
@@ -3,7 +3,7 @@ package service_authz
 import (
 	"github.com/forbearing/gst/database"
 	"github.com/forbearing/gst/logger"
-	model_authz "github.com/forbearing/gst/model/modelauthz"
+	modelauthz "github.com/forbearing/gst/model/authz"
 	"github.com/forbearing/gst/service"
 	"github.com/forbearing/gst/types"
 	"github.com/forbearing/gst/types/consts"
@@ -11,7 +11,7 @@ import (
 )
 
 type userRole struct {
-	service.Base[*model_authz.UserRole, *model_authz.UserRole, *model_authz.UserRole]
+	service.Base[*modelauthz.UserRole, *modelauthz.UserRole, *modelauthz.UserRole]
 }
 
 func init() {
@@ -19,20 +19,20 @@ func init() {
 }
 
 // DeleteAfter support filter and delete multiple user_roles by query parameter `user` and `role`.
-func (r *userRole) DeleteAfter(ctx *types.ServiceContext, userRole *model_authz.UserRole) error {
+func (r *userRole) DeleteAfter(ctx *types.ServiceContext, userRole *modelauthz.UserRole) error {
 	log := logger.Service.WithServiceContext(ctx, consts.PHASE_DELETE_AFTER)
 	user := ctx.URL.Query().Get("user")
 	role := ctx.URL.Query().Get("role")
 
-	userRoles := make([]*model_authz.UserRole, 0)
-	if err := database.Database[*model_authz.UserRole](ctx.DatabaseContext()).WithQuery(&model_authz.UserRole{User: user, Role: role}).WithLimit(-1).List(&userRoles); err != nil {
+	userRoles := make([]*modelauthz.UserRole, 0)
+	if err := database.Database[*modelauthz.UserRole](ctx.DatabaseContext()).WithQuery(&modelauthz.UserRole{User: user, Role: role}).WithLimit(-1).List(&userRoles); err != nil {
 		log.Error(err)
 		return err
 	}
 	for _, rb := range userRoles {
 		log.Infoz("will delete user role", zap.Object("user_role", rb))
 	}
-	if err := database.Database[*model_authz.UserRole](ctx.DatabaseContext()).WithLimit(-1).WithPurge().Delete(userRoles...); err != nil {
+	if err := database.Database[*modelauthz.UserRole](ctx.DatabaseContext()).WithLimit(-1).WithPurge().Delete(userRoles...); err != nil {
 		return err
 	}
 
