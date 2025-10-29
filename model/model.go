96d9641969db59e5b97d14daa79faec6a9c5b4eb
hybfkuf <condryhebenstreit@gmail.com>
2025-10-29 16:20:52 +0800

    feat(model): add model.Any used to database transactions

--- model/model.go
+++ model/model.go
@@ -258,7 +258,7 @@ func (Empty) SetID(id ...string)         {}
 func (Empty) ClearID()                   {}
 func (Empty) Expands() []string          { return nil }
 func (Empty) Excludes() map[string][]any { return nil }
-func (Empty) Purge() bool                { return false } // Default to soft delete
+func (Empty) Purge() bool                { return false }
 func (Empty) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	return nil
 }
@@ -273,3 +273,51 @@ func (Empty) ListBefore(*types.ModelContext) error   { return nil }
 func (Empty) ListAfter(*types.ModelContext) error    { return nil }
 func (Empty) GetBefore(*types.ModelContext) error    { return nil }
 func (Empty) GetAfter(*types.ModelContext) error     { return nil }
+
+// Any is a special placeholder model type used for database transactions
+// when you don't need to specify a concrete model type.
+//
+// Usage example:
+//
+//	err = database.Database[*model.Any](ctx.DatabaseContext()).TransactionFunc(func(tx any) error {
+//	    // Perform database operations within transaction
+//	    files := make([]*namespace.File, 0)
+//	    if err = database.Database[*namespace.File](ctx.DatabaseContext()).
+//	        WithTx(tx).
+//	        WithQuery(&namespace.File{Format: namespace.FileFormat("kv")}).
+//	        List(&files); err != nil {
+//	        return err
+//	    }
+//	    for _, f := range files {
+//	        f.Format = namespace.FileFomatShell
+//	    }
+//	    return database.Database[*namespace.File](ctx.DatabaseContext()).
+//	        WithSelect("format").
+//	        WithTx(tx).
+//	        Update(files...)
+//	})
+//
+// Note:
+//   - Any does not correspond to any database table
+//   - It's only used as a type parameter for generic database operations
+//   - Unlike model.Empty, model.Any is specifically for transaction placeholders
+type Any struct{}
+
+func (Any) GetTableName() string       { return "" }
+func (Any) GetCreatedBy() string       { return "" }
+func (Any) GetUpdatedBy() string       { return "" }
+func (Any) GetCreatedAt() time.Time    { return time.Time{} }
+func (Any) GetUpdatedAt() time.Time    { return time.Time{} }
+func (Any) SetCreatedBy(s string)      {}
+func (Any) SetUpdatedBy(s string)      {}
+func (Any) SetCreatedAt(t time.Time)   {}
+func (Any) SetUpdatedAt(t time.Time)   {}
+func (Any) GetID() string              { return "" }
+func (Any) SetID(id ...string)         {}
+func (Any) ClearID()                   {}
+func (Any) Expands() []string          { return nil }
+func (Any) Excludes() map[string][]any { return nil }
+func (Any) Purge() bool                { return false }
+func (Any) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	return nil
+}
